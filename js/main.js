9980261f8d8343f9520dfd3137a1837359c3b0e1
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-08-18 06:33:27 +0000

    mobile menu refactored with JS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,69 @@
-const toggleClick = () => {
-  // console.log('bang');
+const toggleButton = document.querySelector('#hamburger');
+const logo = document.querySelector('.logo');
+const menuItems = document.querySelector('.menu-items');
+const menuItemUL = document.querySelector('.menu-item-ul');
+const line1 = document.querySelector('#line1');
+const line2 = document.querySelector('#line2');
+const line3 = document.querySelector('#line3');
+
+let clicked = false;
+
+const showLogo = () => {
+  logo.className = 'logo';
 };
 
-const toggleButton = document.querySelector('#hamburger');
-toggleButton.addEventListener('click', toggleClick);
+const hideLogo = () => {
+  logo.className = 'hidden';
+};
+
+const showMenuItems = () => {
+  menuItems.className = 'translate';
+};
+
+const hideMenuItems = () => {
+  menuItems.className = 'menu-items';
+};
+
+const linesClicked = () => {
+  line1.className = 'line line1 line1-clicked';
+  line2.className = 'line line2 line2-clicked';
+  line3.className = 'line line3 line3-clicked';
+};
+
+const linesUnclicked = () => {
+  line1.className = 'line line1';
+  line2.className = 'line line2';
+  line3.className = 'line line3';
+};
+
+const toggleClicked = () => {
+  hideLogo();
+  showMenuItems();
+  linesClicked();
+};
+
+const toggleUnclicked = () => {
+  showLogo();
+  hideMenuItems();
+  linesUnclicked();
+};
+
+const toggleClickEventHandler = () => {
+  clicked = !clicked;
+  if (clicked === true) {
+    toggleClicked();
+  } else {
+    toggleUnclicked();
+  }
+};
+
+toggleButton.addEventListener('click', toggleClickEventHandler);
+
+const menuItemClicked = () => {
+  if (event.target.tagName === 'A') {
+    toggleUnclicked();
+    clicked = false;
+  }
+};
+
+menuItemUL.addEventListener('click', menuItemClicked);
